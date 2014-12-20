05150e9f82a1bccd13dae828ba2794a74a58fa1c
witkowski01 <witkowski01@wp.pl>
2014-12-20 02:02:49 +0100

    Kodowanie do naprawy

--- Steganografia/Steganografia/MainWindow.xaml.cs
+++ Steganografia/Steganografia/MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Drawing;
+using Microsoft.Win32;
+using System.Drawing;
+
 
 namespace Steganografia
 {
@@ -24,13 +27,16 @@ namespace Steganografia
     {
         public MainWindow()
         {
+            
             InitializeComponent();
-
+            
         }
 
-        public void encryption()
-{
-    var originalbmp = new Bitmap(Bitmap.FromFile("steganografia.png")); //Actual image used to encrypt the message
+        #region Kodowanie i dekodowanie
+        public void encryption() // kodowanie
+        {
+            var originalbmp = new Bitmap(Bitmap.FromFile(Sciezka_obrazu.Content.ToString()));
+    //var originalbmp = new Bitmap(Bitmap.FromFile("steganografia.png")); //Actual image used to encrypt the message
            
       var encryptbmp = new Bitmap(originalbmp.Width, originalbmp.Height); // To hold the encrypted image     
 
@@ -39,25 +45,29 @@ namespace Steganografia
       foreach (char character in originalText)
       {
              int asciiValue = Convert.ToInt16(character); // Convert the character to ASCII
-             var firstDigit = asciiValue / 1000; // Extract the first digit of the ASCII
-             var secondDigit = (asciiValue - (firstDigit * 1000)) / 100; //Extract the second digit of the ASCII
-             var thirdDigit = (asciiValue - ((firstDigit * 1000) + (secondDigit * 100)))/10;//Extract the third digit of the ASCII
-             var fourthDigit = (asciiValue - ((firstDigit * 1000) + (secondDigit * 100)+(thirdDigit * 10))); //Extract the third digit of the ASCII
-             ascii.Add(firstDigit); // Add the first digit of the ASCII
-             ascii.Add(secondDigit); // Add the second digit of the ASCII
-             ascii.Add(thirdDigit); // Add the third digit of the ASCII
-             ascii.Add(fourthDigit ); // Add the fourth digit of the ASCII
+             var firstZnak = asciiValue / 1000; // Extract the first Znak of the ASCII
+             var secondZnak = (asciiValue - (firstZnak * 1000)) / 100; //Extract the second Znak of the ASCII
+             var thirdZnak = (asciiValue - ((firstZnak * 1000) + (secondZnak * 100)))/10;//Extract the third Znak of the ASCII
+             var fourthZnak = (asciiValue - ((firstZnak * 1000) + (secondZnak * 100)+(thirdZnak * 10))); //Extract the third Znak of the ASCII
+             ascii.Add(firstZnak); // Add the first Znak of the ASCII
+             ascii.Add(secondZnak); // Add the second Znak of the ASCII
+             ascii.Add(thirdZnak); // Add the third Znak of the ASCII
+             ascii.Add(fourthZnak ); // Add the fourth Znak of the ASCII
      }
      var count = 0; // Have a count
+            int numer = 0;
      for (int row = 0; row < originalbmp.Width; row++) // Indicates row number
      {
            for (int column = 0; column < originalbmp.Height; column++) // Indicate column number
            {
                  var color = originalbmp.GetPixel(row, column); // Get the pixel from each and every row and column
-                 encryptbmp.SetPixel(row, column, System.Drawing.Color.FromArgb(color.A -((count < ascii .Count ) ? ascii[count]:0), color)); // Set ascii value in A of the pixel
+               
+                 encryptbmp.SetPixel(row, column, System.Drawing.Color.FromArgb(color.A -((count < ascii.Count ) ? ascii[count]:0))); // Set ascii value in A of the pixel
+               count++;
            }
+           
     }
-    encryptbmp.Save("EncryptedImage.png", ImageFormat.Png); // Save the encrypted image   
+    encryptbmp.Save("Kodowany.jpg", ImageFormat.Jpeg); // Save the encrypted image   
 }
 
         private void decryption()
@@ -65,40 +75,65 @@ namespace Steganografia
             var characterValue = 0; // Have a variable to store the ASCII value of the character
             string encryptedText = string.Empty; // Have a variable to store the encrypted text
             var ascii = new List<int>(); // Have a collection to store the collection of ASCII
-            var encryptbmp = new Bitmap(Bitmap.FromFile("EncryptedImage.png")); // Load the transparent image
+            var encryptbmp = new Bitmap(Bitmap.FromFile("Kodowany.jpg")); // Load the transparent image
 
             for (int row = 0; row < encryptbmp.Width; row++) // Indicates row number
             {
                 for (int column = 0; column < encryptbmp.Height; column++) // Indicate column number
                 {
                     var color = encryptbmp.GetPixel(row, column); // Get the pixel from each and every row and column
+                    //ascii.Add(255 - color.B);
                     ascii.Add(255 - color.A); // Get the ascii value from A value since 255 is default value
                 }
             }
             for (int i = 0; i < ascii.Count; i++)
             {
                 characterValue = 0;
-                characterValue += ascii[i] * 1000; // Get the first digit of the ASCII value of the encrypted character
+                characterValue += ascii[i] * 1000; // Get the first Znak of the ASCII value of the encrypted character
                 i++;
-                characterValue += ascii[i] * 100; // Get the second digit of the ASCII value of the encrypted character
+                characterValue += ascii[i] * 100; // Get the second Znak of the ASCII value of the encrypted character
                 i++;
-                characterValue += ascii[i] * 10;  // Get the first third digit of the ASCII value of the encrypted character
+                characterValue += ascii[i] * 10;  // Get the first third Znak of the ASCII value of the encrypted character
                 i++;
-                characterValue += ascii[i]; // Get the first fourth digit of the ASCII value of the encrypted character
+                characterValue += ascii[i]; // Get the first fourth Znak of the ASCII value of the encrypted character
                 if (characterValue != 0)
                     encryptedText += char.ConvertFromUtf32(characterValue); // Convert the ASCII characterValue into character
             }
             MessageBox.Show(encryptedText); // Showing the encrypted message in message box
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        #endregion
+
+        private void Kodowanie_click(object sender, RoutedEventArgs e)
+        {
+            encryption();
+        }
+
+        private void Odkodowanie_click(object sender, RoutedEventArgs e)
         {
             decryption();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Wczytaj_obraz(object sender, RoutedEventArgs e)
         {
-            encryption();
+            OpenFileDialog dlg = new OpenFileDialog();
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+
+            //dlg.InitialDirectory = "c:\\Users\\"+userName+"\\Desktop";
+            dlg.Filter = "Image files (*.jpg , *.png , *.bmp) | *.jpg;  *.png;  *.bmp |All Files (*.*)|*.*";
+            dlg.RestoreDirectory = true;
+
+            if (dlg.ShowDialog() == true)
+            {
+                string selectedFileName = dlg.FileName;
+                Sciezka_obrazu.Content = selectedFileName;
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(selectedFileName);
+                bitmap.EndInit();
+                Obraz.Source = bitmap;
+            }
+            
         }
     }
 }
